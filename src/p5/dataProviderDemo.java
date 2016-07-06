7c03162202744c0fa2e2b0ceb5fce5b26ca1ba2a
shane shen <shenjing8412@sina.com>
2016-07-06 14:18:01 +0800

    add code about 9,10zhang

--- src/p5/dataProviderDemo.java
+++ src/p5/dataProviderDemo.java
@@ -1,4 +1,9 @@
 package p5;
+/**
+ * 5.6 TestNG数据提供者DataProvider
+ * TestNG除了可以通过参数化传递数据以外，还有个数据提供者，也是通过注释方式来传递的，
+ * 标准格式是：@DataProvider(name = "XXXX")
+ */
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 public class dataProviderDemo {
